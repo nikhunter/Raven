aa0e2936b281c8c531c3f7849aa63fa89b4f0f25
Niklas W. Micheelsen <niklas@micheelsen.com>
2017-05-18 14:15:12 +0200

    Desktop-First commit

---
+++ WPF/Raven/Raven/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Maps.MapControl.WPF;
+using Raven.Program;
+
+namespace Raven {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        public static ObservableCollection<string> TripTileCollection { get; set; } = new ObservableCollection<string>(); // Collection of Map  pins, uses custom Datapoint class as type
+        public static ObservableCollection<Datapoint> PushPinCollection { get; set; } = new ObservableCollection<Datapoint>(); // Collection of Map  pins, uses custom Datapoint class as type
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            TripTileCollection.Add("One");
+            TripTileCollection.Add("Two");
+            TripTileCollection.Add("Three");
+            TripTileCollection.Add("Four");
+            TripTileCollection.Add("Five");
+
+            TripTileCollection.Add("Six");
+            TripTileCollection.Add("Seven");
+            TripTileCollection.Add("Eight");
+            TripTileCollection.Add("Nine");
+            TripTileCollection.Add("Ten");
+        }
+    }
+}
---
+++ WPF/Raven/Raven/Program/Datapoint.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Maps.MapControl.WPF;
+
+namespace Raven.Program {
+    public class Datapoint {
+        public Location Location { get; set; }
+        public string Date { get; set; }
+        public string Time { get; set; }
+        public int Speed { get; set; }
+        public int Rpm { get; set; }
+
+        public Datapoint(Location location, string date, string time, int speed, int rpm) {
+            Location = location;
+            Date = date;
+            Time = time;
+            Speed = speed;
+            Rpm = rpm;
+        }
+    }
+}
