0b2e8d08c791797e01e9fd231c63d0b4833d0db4
Niklas W. Micheelsen <niklas@micheelsen.com>
2017-05-23 12:49:50 +0200

    Added new inserts for testing purposes

--- SQL/Create_DB.sql
+++ SQL/Create_DB.sql
@@ -1,7 +1,7 @@
 use raven;
 
 DROP TABLE if exists raven.trips;
-DROP table if exists raven.logins;
+DROP TABLE if exists raven.logins;
 
 CREATE TABLE IF NOT EXISTS `raven`.`logins` (
   `id` INT NOT NULL AUTO_INCREMENT,
@@ -16,6 +16,12 @@ COMMENT = 'Login table for Raven-GPS authentication';
 insert into logins (`reg`,`username`,`password`) values (
     "BK79499",
     "RWejlgaard",
+    "9151440965cf9c5e07f81eee6241c042a7b78e9bb2dd4f928a8f6da5e369cdffdd2b70c70663ee30d02115731d35f1ece5aad9b362aaa9850efa99e3d197212a  -"
+);
+
+insert into logins (`reg`,`username`,`password`) values (
+    "BE70846",
+    "NWMicheelsen",
     "9151440965cf9c5e07f81eee6241c042a7b78e9bb2dd4f928a8f6da5e369cdffdd2b70c70663ee30d02115731d35f1ece5aad9b362aaa9850efa99e3d197212a  -"
 );
 
@@ -48,6 +54,14 @@ insert into trips (`time_started`,`time_ended`,`driver_reg`,`log_file`,`driver_u
     "BK79499",
     "None",
     "RWejlgaard"
+);
+
+insert into trips (`time_started`,`time_ended`,`driver_reg`,`log_file`,`driver_username`) values (
+	STR_TO_DATE('05-22-2017 14:00:00','%m-%d-%Y %H:%i:%s'),
+    STR_TO_DATE('05-22-2017 14:43:25','%m-%d-%Y %H:%i:%s'),
+    "BE70846",
+    "None",
+    "NWMicheelsen"
 );
 
 select * from logins;
