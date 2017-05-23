909fb4770ad180301552eb64d7c2f7ebfc9df7d0
Rasmus Wejlgaard Knusen <wejlgaard@protonmail.com>
2017-05-23 12:04:30 +0200

    removed vin from sql

--- SQL/Create_DB.sql
+++ SQL/Create_DB.sql
@@ -5,17 +5,15 @@ DROP table if exists raven.logins;
 
 CREATE TABLE IF NOT EXISTS `raven`.`logins` (
   `id` INT NOT NULL AUTO_INCREMENT,
-  `vin` VARCHAR(20) NOT NULL,
   `reg` VARCHAR(10) NOT NULL,
   `username` VARCHAR(32) NOT NULL,
   `password` mediumtext NOT NULL,
   PRIMARY KEY (`id`, `username`),
-  UNIQUE INDEX `vin_UNIQUE` (`vin` ASC),
+  UNIQUE INDEX `reg_UNIQUE` (`reg` ASC),
   UNIQUE INDEX `username_UNIQUE` (`username` ASC))
 COMMENT = 'Login table for Raven-GPS authentication';
 
-insert into logins (`vin`,`reg`,`username`,`password`) values (
-	"5YFBPRHE5EP086069",
+insert into logins (`reg`,`username`,`password`) values (
     "BK79499",
     "RWejlgaard",
     "9151440965cf9c5e07f81eee6241c042a7b78e9bb2dd4f928a8f6da5e369cdffdd2b70c70663ee30d02115731d35f1ece5aad9b362aaa9850efa99e3d197212a  -"
@@ -25,16 +23,16 @@ CREATE TABLE `raven`.`trips` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `time_started` DATETIME NOT NULL,
   `time_ended` DATETIME NOT NULL,
-  `driver_vin` VARCHAR(20) NOT NULL,
+  `driver_reg` VARCHAR(20) NOT NULL,
   `log_file` MEDIUMBLOB NOT NULL,
   `driver_username` VARCHAR(32) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE INDEX `id_UNIQUE` (`id` ASC),
-  INDEX `vin_idx` (`driver_vin` ASC),
+  INDEX `reg_idx` (`driver_reg` ASC),
   INDEX `driver_username_idx` (`driver_username` ASC),
-  CONSTRAINT `driver_vin_FK`
-    FOREIGN KEY (`driver_vin`)
-    REFERENCES `raven`.`logins` (`vin`)
+  CONSTRAINT `driver_reg_FK`
+    FOREIGN KEY (`driver_reg`)
+    REFERENCES `raven`.`logins` (`reg`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `driver_username_FK`
@@ -44,10 +42,10 @@ CREATE TABLE `raven`.`trips` (
     ON UPDATE NO ACTION)
 COMMENT = 'Trip table for Raven-GPS';
 
-insert into trips (`time_started`,`time_ended`,`driver_vin`,`log_file`,`driver_username`) values (
+insert into trips (`time_started`,`time_ended`,`driver_reg`,`log_file`,`driver_username`) values (
 	STR_TO_DATE('05-18-2017 12:00:00','%m-%d-%Y %H:%i:%s'),
     STR_TO_DATE('05-18-2017 12:38:00','%m-%d-%Y %H:%i:%s'),
-    "5YFBPRHE5EP086069",
+    "BK79499",
     "None",
     "RWejlgaard"
 );
