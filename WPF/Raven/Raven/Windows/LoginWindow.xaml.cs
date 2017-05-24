5757503d295b474e2ef4378790fdd8351b9b77d1
Niklas W. Micheelsen <niklas@micheelsen.com>
2017-05-24 14:01:21 +0200

    Removed title icon from LoginWindow Added login bypass for ease of development

--- WPF/Raven/Raven/Windows/LoginWindow.xaml.cs
+++ WPF/Raven/Raven/Windows/LoginWindow.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,11 +25,33 @@ namespace Raven.Windows {
         private const string ConnectionString = "Server=raven-gps.com;Database=raven;Uid=root;Pwd=Raven123";
         public bool LoginSuccess;
 
+        [DllImport("user32.dll")]
+        static extern uint GetWindowLong(IntPtr hWnd, int nIndex);
+
+        [DllImport("user32.dll")]
+        static extern int SetWindowLong(IntPtr hWnd, int nIndex, uint dwNewLong);
+
+        private const int GwlStyle = -16;
+
+        private const uint WsSysmenu = 0x80000;
+
+        protected override void OnSourceInitialized(EventArgs e) {
+            IntPtr hwnd = new System.Windows.Interop.WindowInteropHelper(this).Handle;
+            SetWindowLong(hwnd, GwlStyle,
+                GetWindowLong(hwnd, GwlStyle) & (0xFFFFFFFF ^ WsSysmenu));
+
+            base.OnSourceInitialized(e);
+        }
+
         public LoginWindow() {
             InitializeComponent();
         }
 
         private void LoginBtn_Click(object sender, RoutedEventArgs e) {
+            AttemptLogin();
+        }
+
+        private void AttemptLogin() {
             // If login is corrrect changes LoginSuccess to true and closes LoginWindow
             if (AuthenticateLogin(UsernameText.Text, PasswordText.Password)) {
                 LoginSuccess = true;
@@ -43,64 +66,41 @@ namespace Raven.Windows {
         public bool AuthenticateLogin(string username, string password) {
             // Stores hash value of password in hash
             var hash = GetHash(password);
-            
+
             MySqlConnection connection = new MySqlConnection(ConnectionString);
             DataTable dt = new DataTable();
             connection.Open();
 
-            try
-            {
+            try {
                 MySqlCommand command = connection.CreateCommand();
                 command.CommandText = $"SELECT * FROM logins";
 
-                using (MySqlDataReader dr = command.ExecuteReader())
-                {
+                using (MySqlDataReader dr = command.ExecuteReader()) {
                     dt.Load(dr);
 
                     // Returns true using LINQ expression
-                    if (dt.Rows.Cast<DataRow>().Where(variable => variable.Field<string>("username") == username).Any(variable => variable.Field<string>("password") == hash))
-                    {
+                    if (dt.Rows.Cast<DataRow>().Where(variable => variable.Field<string>("username") == username).Any(variable => variable.Field<string>("password") == hash)) {
                         return true;
                     }
                     return false;
                 }
             }
-            catch (MySqlException exception)
-            {
+            catch (MySqlException exception) {
                 MessageBox.Show(exception.ToString());
                 return false;
             }
         }
 
-        // Hashes Password and returns hash value
-        //public static string GetHash(string inputString) {
-        //    // Generates SHA1
-        //    using (var sha1 = new SHA1Managed()) {
-        //        var hash = sha1.ComputeHash(Encoding.UTF8.GetBytes(inputString));
-        //        var sb = new StringBuilder(hash.Length * 2);
-
-        //        foreach (var b in hash) {
-        //            sb.Append(b.ToString("X2"));
-        //        }
-
-        //        // Returns hash value
-        //        return sb.ToString();
-        //    }
-        //}
-
-        public static string GetHash(string inputString)
-        {
-            SHA512 sha512 = SHA512Managed.Create();
+        public static string GetHash(string inputString) {
+            SHA512 sha512 = SHA512.Create();
             byte[] bytes = Encoding.UTF8.GetBytes(inputString);
             byte[] hash = sha512.ComputeHash(bytes);
             return GetStringFromHash(hash);
         }
 
-        private static string GetStringFromHash(byte[] hash)
-        {
+        private static string GetStringFromHash(byte[] hash) {
             StringBuilder result = new StringBuilder();
-            for (int i = 0; i < hash.Length; i++)
-            {
+            for (int i = 0; i < hash.Length; i++) {
                 result.Append(hash[i].ToString("X2"));
             }
             return result.ToString();
@@ -112,6 +112,18 @@ namespace Raven.Windows {
 
         private void PasswordText_LostGotKeyboardFocus(object sender, KeyboardFocusChangedEventArgs e) {
             PasswordText.Password = "Password";
+        }
+
+        private void LoginWindow_OnKeyDown(object sender, KeyEventArgs e) {
+            switch (e.Key) {
+                case Key.Enter:
+                    if (Keyboard.IsKeyDown(Key.RightShift)) {
+                        LoginSuccess = true;
+                        Close();
+                    }
+                    AttemptLogin();
+                    break;
+            }
         }
     }
 }
